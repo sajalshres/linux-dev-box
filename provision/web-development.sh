530e64128694aa4b0643798537851b047a3f6d17
Sajal Shrestha <sajal.shrestha@cotiviti.com>
2020-05-27 11:07:01 +0545

    Added nvm support

--- provision/web-development.sh
+++ provision/web-development.sh
@@ -9,3 +9,6 @@ apt-get update
 apt-get install -y nodejs
 # Install eslint
 npm install -g eslint
+
+# Install nvm as default vagrant user
+runuser -l vagrant -c "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash"
